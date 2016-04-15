0aaa02b1660d5b604622831b5cadd82ad913d9a2
BaptisteLeBescond <baptiste.lb@gmail.com>
2016-04-15 12:22:40 +0200

    Add current_pack to user

--- src/User.java
+++ src/User.java
@@ -3,6 +3,7 @@ import java.util.HashMap;
 
 public class User {
 	private HashMap<Pack, ArrayList<Cigarette>> cigarettes_smoked;
+	private Pack currentPack;
 	
 	public User() {
 		cigarettes_smoked = new HashMap<Pack, ArrayList<Cigarette>>();
