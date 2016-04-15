03a958bb8ca556ab7d2cccf43ef16110f98216c0
BaptisteLeBescond <baptiste.lb@gmail.com>
2016-04-15 12:22:50 +0200

    Add console choices

--- src/Main.java
+++ src/Main.java
@@ -6,30 +6,45 @@ public class Main {
 
 		Scanner scanIn = new Scanner(System.in);
 		
-		System.out.println("Bonjour, veuillez écrire le type de cigarette : ");
-		String packType = scanIn.nextLine();
-		   
-		System.out.println("Veuillez écrire la marque de cigarette : ");
-		String packBrand = scanIn.nextLine();
+		System.out.println("Bonjour, voulez-vous paramétrer un paquet (tapez 1), ou augmenter le compteur (tapez 2) ?");
+		String choice = scanIn.nextLine();
 		
-		System.out.println("Veuillez écrire le prix du paquet de cigarette (en €) : ");
-		float packPrice = Float.parseFloat(scanIn.nextLine());
+		if(choice == "1"){
+			
+			System.out.println("Veuillez écrire le type de cigarette : ");
+			String packType = scanIn.nextLine();
+			   
+			System.out.println("Veuillez écrire la marque de cigarette : ");
+			String packBrand = scanIn.nextLine();
+			
+			System.out.println("Veuillez écrire le prix du paquet de cigarette (en €) : ");
+			float packPrice = Float.parseFloat(scanIn.nextLine());
+			
+			System.out.println("Veuillez écrire le taux de tabac dans la cigarette (en %) : ");
+			int packTobaccoRate = Integer.parseInt(scanIn.nextLine());
+			
+			System.out.println("Veuillez écrire le taux de papier dans la cigarette (en %) : ");
+			int packPaperRate = Integer.parseInt(scanIn.nextLine());
+			
+			System.out.println("Veuillez écrire le taux d'agents de texture et saveur dans la cigarette (en %) : ");
+			int packAgentsRate = Integer.parseInt(scanIn.nextLine());
+			   
+			System.out.println(packType +" "+ packBrand +" "+ packPrice +" "+ packTobaccoRate +" "+ packPaperRate +" "+ packAgentsRate);
+			  
+			scanIn.close();
+			
+			Pack pack1 = new Pack(new Type(packType), packBrand, packPrice, packTobaccoRate, packPaperRate, packAgentsRate, null);
+			
+			System.out.println(pack1);
+			
+		}
 		
-		System.out.println("Veuillez écrire le taux de tabac dans la cigarette (en %) : ");
-		int packTobaccoRate = Integer.parseInt(scanIn.nextLine());
+		else if(choice == "2"){
+			//???
+		}
 		
-		System.out.println("Veuillez écrire le taux de papier dans la cigarette (en %) : ");
-		int packPaperRate = Integer.parseInt(scanIn.nextLine());
-		
-		System.out.println("Veuillez écrire le taux d'agents de texture et saveur dans la cigarette (en %) : ");
-		int packAgentsRate = Integer.parseInt(scanIn.nextLine());
-		   
-		System.out.println(packType +" "+ packBrand +" "+ packPrice +" "+ packTobaccoRate +" "+ packPaperRate +" "+ packAgentsRate);
-		  
-		scanIn.close();
-		
-		Pack pack1 = new Pack(new Type(packType), packBrand, packPrice, packTobaccoRate, packPaperRate, packAgentsRate, null);
-		
-		System.out.println(pack1);
+		else if(choice == "mashallah"){
+			System.out.println("Wallah, wallah");
+		}
 	}
 }
